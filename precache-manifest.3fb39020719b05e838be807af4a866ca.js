275e597eb6a6336f35585f7bd13b722216257048
PavithraGunasekaran <pavithra.g13@gmail.com>
2020-06-16 10:19:21 +0530

    Updates

---
+++ precache-manifest.3fb39020719b05e838be807af4a866ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab0f9a898b1fd4b305b63a5b149dc06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc7e7f7da41455cd1299",
+    "url": "/static/css/main.765ea92c.chunk.css"
+  },
+  {
+    "revision": "af5b99b9878dee53e78a",
+    "url": "/static/js/2.95e655bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.95e655bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc7e7f7da41455cd1299",
+    "url": "/static/js/main.8f316fc8.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
